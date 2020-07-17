56b549e255c4e5bea6dbd440c25979a2047d4426
lianjunting <lianjunting@cmii.chinamobile.com>
2020-07-17 15:10:51 +0800

    chore: 更新注释

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-let events = [];       // 事件数组
-let stopFn;            // 停止录制方法
-let stopEl = null;     // 停止按钮元素
-let inputEl = null;    // 读取 JSON 文件表单元素
-let replayer = null;   // 播放器对象
+let events = []; // 事件数组
+let stopFn; // 停止录制方法
+let stopEl = null; // 停止按钮元素
+let inputEl = null; // 读取 JSON 文件表单元素
+let replayer = null; // 播放器对象
 let playerLayer = null; // 播放器外部容器
 
 /**
  * 播放
- * 创建播放器 DOM 结构
+ * 创建播放器容器和实例
  */
 function replay(eventData) {
   if (!playerLayer) {
@@ -18,7 +18,7 @@ function replay(eventData) {
       <div class="witness-player-close-btn">
         <svg t="1594951950399" class="witness-player-close-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2553" width="128" height="128"><path d="M556.8 512L832 236.8c12.8-12.8 12.8-32 0-44.8-12.8-12.8-32-12.8-44.8 0L512 467.2l-275.2-277.333333c-12.8-12.8-32-12.8-44.8 0-12.8 12.8-12.8 32 0 44.8l275.2 277.333333-277.333333 275.2c-12.8 12.8-12.8 32 0 44.8 6.4 6.4 14.933333 8.533333 23.466666 8.533333s17.066667-2.133333 23.466667-8.533333L512 556.8 787.2 832c6.4 6.4 14.933333 8.533333 23.466667 8.533333s17.066667-2.133333 23.466666-8.533333c12.8-12.8 12.8-32 0-44.8L556.8 512z" p-id="2554"></path></svg>
       </div>
-    `
+    `;
     document.body.appendChild(playerLayer);
     let closeBtn = document.querySelector('.witness-player-close-btn');
     closeBtn.addEventListener('click', () => {
