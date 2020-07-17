dd9e8dc657e0df8cd7f72c250a991debb2a194bb
lianjunting <lianjunting@cmii.chinamobile.com>
2020-07-17 10:16:32 +0800

    feat: 优化

--- js/main.js
+++ js/main.js
@@ -1,37 +1,44 @@
-let events = [];
-let stopFn;
-let stopEl = null;
-let inputEl = null;
-let replayer = null;
-let playerRoot = null;
+let events = [];       // 事件数组
+let stopFn;            // 停止录制方法
+let stopEl = null;     // 停止按钮元素
+let inputEl = null;    // 读取 JSON 文件表单元素
+let replayer = null;   // 播放器对象
+let playerLayer = null; // 播放器外部容器
 
+/**
+ * 播放
+ * 创建播放器 DOM 结构
+ */
 function replay(eventData) {
-  if (!replayer) {
-    let playerLayer = document.createElement('div');
+  if (!playerLayer) {
+    playerLayer = document.createElement('div');
     playerLayer.className = 'witness-player-layer';
     playerLayer.innerHTML = `
       <div class="witness-player-root"></div>
-      <div class="witness-player-close-btn">×</div>
+      <div class="witness-player-close-btn">
+        <svg t="1594951950399" class="witness-player-close-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2553" width="128" height="128"><path d="M556.8 512L832 236.8c12.8-12.8 12.8-32 0-44.8-12.8-12.8-32-12.8-44.8 0L512 467.2l-275.2-277.333333c-12.8-12.8-32-12.8-44.8 0-12.8 12.8-12.8 32 0 44.8l275.2 277.333333-277.333333 275.2c-12.8 12.8-12.8 32 0 44.8 6.4 6.4 14.933333 8.533333 23.466666 8.533333s17.066667-2.133333 23.466667-8.533333L512 556.8 787.2 832c6.4 6.4 14.933333 8.533333 23.466667 8.533333s17.066667-2.133333 23.466666-8.533333c12.8-12.8 12.8-32 0-44.8L556.8 512z" p-id="2554"></path></svg>
+      </div>
     `
     document.body.appendChild(playerLayer);
-    playerRoot = document.querySelector('.witness-player-root');
     let closeBtn = document.querySelector('.witness-player-close-btn');
     closeBtn.addEventListener('click', () => {
       replayer.pause();
-      document.body.removeChild(playerLayer);
-      playerLayer = null;
+      playerLayer.style.display = 'none';
       replayer = null;
-      closeBtn = null;
-      playerRoot = null;
     });
   }
+  playerLayer.style.display = 'block';
+  const playerRoot = document.querySelector('.witness-player-root');
   playerRoot.innerHTML = '';
   replayer = new rrweb.Replayer(eventData, {
-    root: document.querySelector('.witness-player-root'),
+    root: playerRoot,
   });
   replayer.play();
 }
 
+/**
+ * 监听 ContextMenu 点击事件分发的消息，根据类型做出记录与回放操作
+ */
 chrome.extension.onMessage.addListener(function (request) {
   switch (request.action) {
     case 'record':
